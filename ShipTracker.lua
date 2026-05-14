85814364e0916e8f605566438d2cf0c8d9cbda14
Skript <vicenteromero1104@gmail.com>
2026-05-14 00:05:14 -0300

    Create ShipTracker.lua

---
+++ ShipTracker.lua
@@ -0,0 +1,21 @@
+-- startup.lua
+
+local CHANNEL = 50
+local INTERVAL = 30
+local LABEL = os.getComputerLabel() or ("Unit-" .. os.getComputerID())
+
+local modem = peripheral.find("modem")
+
+while true do
+  local x, y, z = gps.locate(5)
+  if x then
+    modem.transmit(CHANNEL, CHANNEL, {
+      label = LABEL,
+      x = math.floor(x),
+      y = math.floor(y),
+      z = math.floor(z),
+      time = os.time()
+    })
+  end
+  os.sleep(INTERVAL)
+end
